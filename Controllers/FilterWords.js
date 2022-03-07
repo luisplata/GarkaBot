6ec1bb47bbc5959719833e334e5ae44626c32f62
PeryLoth <www.luisplata@gmail.com>
2022-03-07 10:22:02 -0500

    error shoot the callback; solved

--- Controllers/FilterWords.js
+++ Controllers/FilterWords.js
@@ -4,16 +4,13 @@ var logger = require("../utilitys/logs").log();
 var Filter = function(word, callback){
     var splitWord = word.split(' ');
     splitWord.forEach(function(value, index, array){
-        console.log(">>>>>>>>>>>>>"+value);
         mysql.Consulta("select * from palabras p where p.palabra like '"+value+"'",function(req){
             logger.info(req);
             if(req.length > 0){
-                console.log("---------"+value);
                 callback("the word '"+word+"' is forbidden");
             }
         },function (err) {
             logger.error(err);
-            callback("error type "+err);
         });
     });
 }
