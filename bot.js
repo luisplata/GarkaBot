1ddaf18b1f747aa3d57b86bcda37ed9510b4ab26
PeryLoth <www.luisplata@gmail.com>
2022-03-07 09:54:34 -0500

    adding left files

---
+++ bot.js
@@ -0,0 +1,26 @@
+const TelegramBot = require('node-telegram-bot-api');
+var entorno = require('./utilitys/config-modules.js').config();
+const bot = new TelegramBot(entorno.key, {polling:true});
+const wordController = require("./Controllers/AddWordFobidden.js");
+const filter = require("./Controllers/FilterWords.js");
+bot.on('polling_error', function(error){
+    console.log(error);
+});
+bot.onText(/^\/addWord (.+)/, function(msg){
+    var chatId = msg.chat.id;
+    const resp = msg.text.split(' ')[1];
+
+    wordController.AddWord(resp, function (message) {
+        console.log(message);
+        bot.sendMessage(chatId, message);
+    });
+});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const resp = msg.text;
+    console.log(msg);
+    filter.Filter(resp, function (message) {
+        bot.deleteMessage(chatId, msg.message_id, form = {});
+    });
+});
---
+++ utilitys/conexion.js
@@ -0,0 +1,24 @@
+var mysql      	= require('mysql');
+var entorno 	= require('../utilitys/config-modules.js').config();
+
+var consulta 	= function(consultica,callback, callbackError){
+	var connection = mysql.createConnection({
+	  host     : entorno.db.servidor,
+	  user     : entorno.db.usuario,
+	  password : entorno.db.password
+	});
+
+	connection.connect();
+	connection.query('USE '+entorno.db.database);
+	connection.query(consultica, function(err, rows, fields) {
+	  if (err){
+		  callbackError(err);
+		  return;
+	  }
+	  callback(rows);
+	});
+
+	connection.end();
+}
+
+module.exports.Consulta = consulta;
